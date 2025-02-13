ae019bcfa43603b7b619de1bd2bc5aada01636d7
HodonoYusuke0924 <136606754+HodonoYusuke0924@users.noreply.github.com>
2025-02-13 18:20:53 +0900

    p

---
+++ Assets/Script/Common/alllow/bulet.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class bulet : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] public InputActionReference shootbutton;
+
+    private void Update()
+    {
+        if (shootbutton.action.)
+        {
+
+        }
+        Debug.Log("a");
+    }
+
+
+}
