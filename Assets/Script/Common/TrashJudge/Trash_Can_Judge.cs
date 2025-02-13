01b601b4e2ad386f5fdb65efc6df7208d68dfa14
SatoShinya1 <c612101397@tokushima-u.ac.jp>
2025-02-13 17:03:35 +0900

    push

--- Assets/Script/Common/TrashJudge/Trash_Can_Judge.cs
+++ Assets/Script/Common/TrashJudge/Trash_Can_Judge.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Scenes;
 
 public class Trash_Can_Judge : MonoBehaviour
 {
---
+++ Assets/Script/Scenes/GameManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Scenes
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+        public int count;
+    }
+}
